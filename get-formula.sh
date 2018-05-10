25ddfd93a0314f2c9abfb4b882ee47c841e6ff57
Nitay Joffe <nitay@actioniq.com>
2018-05-10 15:48:52 -0400

    add pkgs

---
+++ get-formula.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+if [[ $# -eq 0 ]]; then
+  echo "Pull down latest homebrew formula"  
+  echo
+  echo "Usage: $0 <name>"
+  echo "  e.g $0 packer"
+  exit
+fi
+
+base_dir=$(cd "$(dirname "$0")" && pwd)
+formula_dir=$base_dir/Formula
+
+pushd $formula_dir &>/dev/null
+wget https://raw.githubusercontent.com/Homebrew/homebrew-core/master/Formula/$1.rb
+popd &>/dev/null
