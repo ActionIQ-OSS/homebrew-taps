ac95f2c2ea3a8645cca61b163b48e3208955a864
Mitesh Patel <miteshp@live.com>
2019-03-18 16:22:09 -0500

    Fix

--- Formula/packer.rb
+++ Formula/packer.rb
@@ -1,40 +1,73 @@
+require "language/go"
+
 class Packer < Formula
   desc "Tool for creating identical machine images for multiple platforms"
   homepage "https://packer.io"
   url "https://github.com/hashicorp/packer.git",
-      :tag      => "v1.3.5",
-      :revision => "542cb1d872b4fbba57f34ebb5913a266017a38df"
+      :tag => "v1.0.0",
+      :revision => "e3d65a193e96c7b228589fc9699e39998ec12e4e"
   head "https://github.com/hashicorp/packer.git"
 
   bottle do
     root_url "https://homebrew.bintray.com/bottles"
     cellar :any_skip_relocation
-    sha256 "d89789a2b9eb05c72f95c160c218a1b34281de16940cecb906c0b87ff6c04f30" => :mojave
-    sha256 "4ed4109eb62477592281fb36a9ab543265fdb70916095140f9c488f06550ad3c" => :high_sierra
-    sha256 "8168a91a5748a5f673f714a398d648a6197c45ee1565a9718439c0a4ae894e31" => :sierra
+    sha256 "729ecbb484c2e6575ee8f25e3842cc9342cb5785d04e6f842c58ceb9a7c19610" => :sierra
+    sha256 "b80c62c882784f5f11df70a10d5ed3708b2b88c72bd9c435f5691710536bc32a" => :el_capitan
+    sha256 "1ebf37130fba12a90163c06eb4b5900e2fd538c0431782e0c175034cf8de71f0" => :yosemite
   end
 
-  depends_on "coreutils" => :build
+  depends_on "hg" => :build
   depends_on "go" => :build
   depends_on "govendor" => :build
-  depends_on "gox" => :build
+
+  go_resource "github.com/mitchellh/gox" do
+    url "https://github.com/mitchellh/gox.git",
+        :revision => "c9740af9c6574448fd48eb30a71f964014c7a837"
+  end
+
+  go_resource "github.com/mitchellh/iochan" do
+    url "https://github.com/mitchellh/iochan.git",
+        :revision => "87b45ffd0e9581375c491fef3d32130bb15c5bd7"
+  end
+
+  go_resource "golang.org/x/tools" do
+    url "https://go.googlesource.com/tools.git",
+        :revision => "cbb995d093b2c4a12ac074e53d90373ecc827527"
+  end
 
   def install
     ENV["XC_OS"] = "darwin"
-    ENV["XC_ARCH"] = "amd64"
+    ENV["XC_ARCH"] = MacOS.prefer_64_bit? ? "amd64" : "386"
     ENV["GOPATH"] = buildpath
+    # For the gox buildtool used by packer, which
+    # doesn't need to be installed permanently.
+    ENV.append_path "PATH", buildpath
 
-    packerpath = buildpath/"src/github.com/hashicorp/packer"
+    # TODO: remove "else" (mitchellh path) on next release
+    if build.head?
+      packerpath = buildpath/"src/github.com/hashicorp/packer"
+    else
+      packerpath = buildpath/"src/github.com/mitchellh/packer"
+    end
     packerpath.install Dir["{*,.git}"]
+    Language::Go.stage_deps resources, buildpath/"src"
+
+    cd "src/github.com/mitchellh/gox" do
+      system "go", "build"
+      buildpath.install "gox"
+    end
+
+    cd "src/golang.org/x/tools/cmd/stringer" do
+      system "go", "build"
+      buildpath.install "stringer"
+    end
 
     cd packerpath do
-      # Avoid running `go get`
+      # We handle this step above. Don't repeat it.
       inreplace "Makefile" do |s|
-        s.gsub! "go get github.com/kardianos/govendor", ""
         s.gsub! "go get github.com/mitchellh/gox", ""
-        s.gsub! "go get -u github.com/mna/pigeon", ""
-        s.gsub! "go get golang.org/x/tools/cmd/goimports", ""
         s.gsub! "go get golang.org/x/tools/cmd/stringer", ""
+        s.gsub! "go get github.com/kardianos/govendor", ""
       end
 
       (buildpath/"bin").mkpath
@@ -51,7 +84,7 @@ class Packer < Formula
 
   test do
     minimal = testpath/"minimal.json"
-    minimal.write <<~EOS
+    minimal.write <<-EOS.undent
       {
         "builders": [{
           "type": "amazon-ebs",
@@ -70,6 +103,6 @@ class Packer < Formula
         }]
       }
     EOS
-    system "#{bin}/packer", "validate", "-syntax-only", minimal
+    system "#{bin}/packer", "validate", minimal
   end
 end
